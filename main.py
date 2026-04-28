ab8f140f26379831f53a8634665a506bc8c9429b
gameofedelweissbestpart <gameofedelweiss@gmail.com>
2026-04-28 21:12:59 +0000

    ประจำวัน done

--- main.py
+++ main.py
@@ -347,7 +347,7 @@ class AdminPanelView(discord.ui.View):
 async def daily_report_task():
     n = get_thai_time()
     # รายวัน 00:05 น.
-    if n.hour == 4 and n.minute == 5:
+    if n.hour == 4 and n.minute == 15:
         cfg = load_json(CONFIG_PATH, {})
         ch_id = cfg.get("daily_ch", 0)
         if ch_id:
@@ -373,8 +373,8 @@ async def daily_report_task():
                 separator = "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
                 
                 em = discord.Embed(
-                    title=f"📋 รายงานสรุปการลาประจำวัน",
-                    description=f"**📅 ของวันที่ {yesterday.strftime('%d/%m/%Y')}**\n{separator}",
+                    title=f"**📋 __รายงานสรุปการลาประจำวัน__**",
+                    description=f"**📅 วันที่ {yesterday.strftime('%d/%m/%Y')}**\n{separator}",
                     color=0x9b59b6 if ac else 0x2ecc71
                 )
 
@@ -390,12 +390,12 @@ async def daily_report_task():
                         # แสดงผล: 👤 ชื่อ — [ประเภท]
                         msg += f"👤 **{tn}** — ` {cat} `\n"
                         # แสดงผล: ┗ 💬 เหตุผล
-                        msg += f"┗ 💬 เหตุผล: {i['reason']}"
+                        msg += f"┗ เหตุผล: {i['reason']}"
                         
                         if i['user_id'] != i['target_id']:
                             su = ch.guild.get_member(int(i['user_id']))
                             sn = su.display_name if su else f"ID: {i['user_id']}"
-                            msg += f" *(ผู้ลาแทน: @{sn})*"
+                            msg += f" *(ผู้ลาแทน: {sn})*"
                         msg += "\n\n"
                     
                     # รายชื่อคนลา
